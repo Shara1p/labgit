ea864d4bf190e30381d8bfd2a104e35778450ad3
Sharap <toptricker8@gmail.com>
2023-06-13 18:21:34 +0300

    Добавлен основной функционал

--- labgit/Program.cs
+++ labgit/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,40 @@ namespace labgit
     {
         static void Main(string[] args)
         {
+            var inFile = new StreamReader("numbers.txt");
+
+            var text = inFile.ReadToEnd();
+            string[] numbers = text.Split(' ');
+
+            List<int> nums = new List<int>();
+            List<int> nums2 = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                try
+                {
+                    nums.Add(Convert.ToInt32(num));
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > 0 && nums[i] % 2 == 0)
+                {
+                    nums2.Add(nums[i]);
+                }
+            }
+
+            foreach (var num in nums2)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
         }
     }
 }
